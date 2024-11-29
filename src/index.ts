98d59cd39f40f79816d667f216a2b69501aae7f4
KazariEX <1364035137@qq.com>
2024-11-30 02:34:30 +0800

    feat: implement `get`, `set`, `has` and `delete`

---
+++ src/index.ts
@@ -0,0 +1,172 @@
+type State =
+    | "dot"
+    | "bracket"
+    | "property";
+
+export function splitProperty(path: string) {
+    const states: State[] = [];
+    const chars = path.trim().split(/\s*/);
+
+    const props: string[] = [];
+    let prop = "";
+
+    let escape = false;
+    let next = init;
+    let i = 0;
+    while (i < chars.length) {
+        const char = chars[i];
+        next = next(char);
+    }
+
+    if (states.at(-1) === "property") {
+        exit("property");
+    }
+    return props;
+
+    function init(char: string) {
+        if (char === "[") {
+            consume(char);
+            enter("bracket");
+            enter("property");
+            return bracket;
+        }
+        enter("dot");
+        enter("property");
+        return dot;
+    }
+
+    function start(char: string) {
+        if (char === ".") {
+            consume(char);
+            enter("dot");
+            enter("property");
+            return dot;
+        }
+        if (char === "[") {
+            consume(char);
+            enter("bracket");
+            enter("property");
+            return bracket;
+        }
+        throw new Error(`[Propathy] Not allowed to start with characters other than "." or "[".`);
+    }
+
+    function dot(char: string) {
+        if (!escape && (char === "." || char === "[")) {
+            exit("property");
+            exit("dot");
+            return start;
+        }
+        consume(char);
+        return dot;
+    }
+
+    function bracket(char: string) {
+        if (!escape && char === "]") {
+            exit("property");
+            exit("bracket");
+            consume(char);
+            return start;
+        }
+        consume(char);
+        return bracket;
+    }
+
+    function consume(char: string) {
+        i++;
+        if (states.at(-1) === "property") {
+            if (escape) {
+                escape = false;
+                prop += char;
+            }
+            else if (char === "\\") {
+                escape = true;
+            }
+            else {
+                prop += char;
+            }
+        }
+    }
+
+    function enter(state: State) {
+        states.push(state);
+    }
+
+    function exit(state: State) {
+        if (state !== states.at(-1)) {
+            throw new Error(`[Propathy] "${state}" is not the last state.`);
+        }
+        if (state === "property") {
+            if (escape) {
+                escape = false;
+                prop += "\\";
+            }
+            props.push(prop);
+            prop = "";
+        }
+        states.pop();
+    }
+}
+
+export function getProperty(target: Record<string, any>, path: string, defaultValue?: unknown) {
+    const props = splitProperty(path);
+
+    let obj = target;
+    for (const prop of props) {
+        obj = obj[prop];
+        if (!obj) {
+            return defaultValue;
+        }
+    }
+    return obj;
+}
+
+export function setProperty(target: Record<string, any>, path: string, value: unknown) {
+    const props = splitProperty(path);
+    if (!props.length) {
+        return;
+    }
+
+    let obj = target;
+    for (let i = 0; i < props.length - 1; i++) {
+        obj = (
+            obj[props[i]] ??= /^\d+$/.test(props[i + 1]) ? [] : {}
+        );
+    }
+    obj[props.at(-1)!] = value;
+}
+
+export function hasProperty(target: Record<string, any>, path: string) {
+    const props = splitProperty(path);
+
+    let obj = target;
+    for (let i = 0; i < props.length; i++) {
+        const prop = props[i];
+        if (prop in obj) {
+            obj = obj[prop];
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
+
+export function deleteProperty(target: Record<string, any>, path: string) {
+    const props = splitProperty(path);
+    if (!props.length) {
+        return false;
+    }
+
+    let obj = target;
+    for (let i = 0; i < props.length; i++) {
+        const prop = props[i];
+        if (prop in obj) {
+            if (i === props.length - 1) {
+                return delete obj[prop];
+            }
+            obj = obj[prop];
+            continue;
+        }
+        return false;
+    }
+}
---
+++ test/index.test.ts
@@ -0,0 +1,116 @@
+import { describe, expect, it } from "vitest";
+import { deleteProperty, getProperty, hasProperty, setProperty, splitProperty } from "../src";
+
+describe("split", () => {
+    it("normal", () => {
+        const path = "foo.bar[0].baz";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["foo", "bar", "0", "baz"]);
+    });
+
+    it("escape", () => {
+        const path = "foo\\..bar\\[0\\].baz\\";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["foo.", "bar[0]", "baz\\"]);
+    });
+
+    it("bracket follow bracket", () => {
+        const path = "foo.bar[0][baz]";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["foo", "bar", "0", "baz"]);
+    });
+
+    it("start with bracket", () => {
+        const path = "[0].foo.bar";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["0", "foo", "bar"]);
+    });
+
+    it("end with dot", () => {
+        const path = "foo.bar.";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["foo", "bar", ""]);
+    });
+
+    it("end with empty bracket", () => {
+        const path = "foo.bar[]";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["foo", "bar", ""]);
+    });
+
+    it("end without right bracket", () => {
+        const path = "foo.bar[baz";
+        const props = splitProperty(path);
+        expect(props).toStrictEqual(["foo", "bar", "baz"]);
+    });
+});
+
+describe("get", () => {
+    it("normal", () => {
+        const obj = { foo: { bar: [{ baz: "qux" }] } };
+        const path = "foo.bar[0].baz";
+        expect(getProperty(obj, path)).toBe("qux");
+    });
+
+    it("with default value", () => {
+        const obj = { foo: {} };
+        const path = "foo.bar[0].baz";
+        expect(getProperty(obj, path, "qux")).toBe("qux");
+    });
+});
+
+describe("set", () => {
+    it("normal", () => {
+        const obj = { foo: { bar: [{}] } };
+        const path = "foo.bar[0].baz";
+        const value = "qux";
+        setProperty(obj, path, value);
+        expect(JSON.stringify(obj)).toBe(
+            `{"foo":{"bar":[{"baz":"qux"}]}}`
+        );
+    });
+
+    it("with recursion", () => {
+        const obj = {};
+        const path = "foo.bar[0].baz";
+        const value = "qux";
+        setProperty(obj, path, value);
+        expect(JSON.stringify(obj)).toBe(
+            `{"foo":{"bar":[{"baz":"qux"}]}}`
+        );
+    });
+});
+
+describe("has", () => {
+    it("true", () => {
+        const obj = { foo: { bar: [{ baz: "qux" }] } };
+        const path = "foo.bar[0].baz";
+        expect(hasProperty(obj, path)).toBeTruthy();
+    });
+
+    it("false", () => {
+        const obj = { foo: { bar: [{ baz: "qux" }] } };
+        const path = "foo.baz[0].bar";
+        expect(hasProperty(obj, path)).toBeFalsy();
+    });
+});
+
+describe("delete", () => {
+    it("true", () => {
+        const obj = { foo: { bar: [{ baz: "qux" }] } };
+        const path = "foo.bar[0].baz";
+        expect(deleteProperty(obj, path)).toBeTruthy();
+        expect(JSON.stringify(obj)).toBe(
+            `{"foo":{"bar":[{}]}}`
+        );
+    });
+
+    it("false", () => {
+        const obj = { foo: { bar: [{ baz: "qux" }] } };
+        const path = "foo.bar[1]";
+        expect(deleteProperty(obj, path)).toBeFalsy();
+        expect(JSON.stringify(obj)).toBe(
+            `{"foo":{"bar":[{"baz":"qux"}]}}`
+        );
+    });
+});
